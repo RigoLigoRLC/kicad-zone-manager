a20410181c70d3eaa23a33a2c6714e78691d56d3
RigoLigoRLC <49363666+RigoLigoRLC@users.noreply.github.com>
2022-01-23 21:05:11 +0800

    Add icon, fix i18n bug

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from .ui_impl import frmMainImpl
 import gettext
 gettext.bindtextdomain('zone_manager', os.path.join(os.path.dirname(__file__), 'lang'))
 gettext.textdomain('zone_manager')
-os.putenv('LC_MESSAGES', __import__('locale').getdefaultlocale()[0])
+os.environ['LC_MESSAGES'] = __import__('locale').getdefaultlocale()[0]
 __import__('builtins').__dict__['_'] = gettext.gettext
 
 class AreaManagerPlugin(pcbnew.ActionPlugin):
@@ -13,6 +13,7 @@ class AreaManagerPlugin(pcbnew.ActionPlugin):
         self.category = _("Modify PCB")
         self.description = _("Manage priority of many copper areas")
         self.show_toolbar_button = True
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), 'icon.png')
 
     def Run(self):
         m_mainWin = frmMainImpl(None)
